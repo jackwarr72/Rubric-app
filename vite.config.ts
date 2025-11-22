c77b044181b10e1e0ae3e37aa88218dffcdfd8aa
Jack Guerra <jackwarr72@gmail.com>
2025-11-22 16:02:41 -0600

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, (process as any).cwd(), '');
   return {
     plugins: [react()],
-    base: './', // Ensures assets are loaded correctly on GitHub Pages
+    base: "/Rubric-app/", // <-- Set to repo name for Github Pages
     define: {
-      // Maps the VITE_API_KEY from .env to process.env.API_KEY in the code
       'process.env.API_KEY': JSON.stringify(env.VITE_API_KEY)
     }
   };
